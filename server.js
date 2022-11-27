2f1e9fbc963f5b0e895076ae3c8a1356336de7e4
Ricky Aponte <Rickyapontephotography@gmail.com>
2022-11-26 22:32:29 -0500

    attached all the files from develop public

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static('Develop/public'));
 
 // HTML get Routes
 app.get('/', (req, res) =>
