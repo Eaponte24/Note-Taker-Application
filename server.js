2f7d77a73e21212639f94897b4c719726ecc66ee
Ricky Aponte <Rickyapontephotography@gmail.com>
2022-11-26 22:17:34 -0500

    cleared up some issues with html

--- server.js
+++ server.js
@@ -36,6 +36,55 @@ app.get('/api/notes', (req, res) => {
     });
   });
 
+  // API post Route
+  app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+  
+    // Destructuring assignment for the items in req.body
+    const {title, text,} = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        review_id: uuid(),
+      };
+  
+      fs.readFile('/api/notes', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const parsedReviews = JSON.parse(data);
+  
+          parsedReviews.push(newNote);
+        
+    
+      // Write the string to a file
+      fs.writeFile(`/api/notes`,
+       JSON.stringify(parsedReviews),
+       (writeErr) => 
+       writeErr
+          ? console.error(writeErr)
+          : console.info('Successfully updated Notes!')
+      );
+    }
+  });
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in posting Note');
+    }
+  });
+
 
   app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
